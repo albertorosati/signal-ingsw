7ec2872ab60c710988412f584db7166bcad43867
Alberto <me@albertorosati.it>
2021-05-20 22:19:18 +0200

    MariaDB Connector First Commit

--- Server/src/database/Connector.java
+++ Server/src/database/Connector.java
@@ -2,9 +2,11 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Connector {
+public class Connector implements AutoCloseable {
 
 	private Connection conn;
 	private final String HOST = "signal-ingsft.duckdns.org";
@@ -20,7 +22,25 @@ public class Connector {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void query() {
+		try {
+			PreparedStatement st = conn.prepareStatement("INSERT INTO (id, person) VALUES (?,?)");
+			st.setString(1, "1");
+			st.setString(2, "alb");
+			//per query di insert, update, delete si usa ->
+			st.executeUpdate();
+			//per query di select si usa ->
+			ResultSet result = st.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
+	@Override
+	public void close() throws Exception {
+		conn.close();
 	}
 
 }
