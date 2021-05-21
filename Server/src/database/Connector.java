d8dfbe1068703a80182e6cb46de101c1cef28344
Alberto <me@albertorosati.it>
2021-05-21 12:05:21 +0200

    Connector Host Fix

--- Server/src/database/Connector.java
+++ Server/src/database/Connector.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 public class Connector implements AutoCloseable {
 
 	private Connection conn;
-	private final String HOST = "signal-ingsft.duckdns.org";
+	private final String HOST = "ingsft-signal.duckdns.org";
 	private final String USERNAME = "admin";
 	private final String PASSWORD = "password";
-	private final String DATABASE = "appdb";
+	private final String DATABASE = "applicazione";
 	private final String URL = "jdbc:mariadb://" + HOST + ":3306/" + DATABASE + "?user=" + USERNAME + "&password="
 			+ PASSWORD;
 
