d8d42027b8aea95e9347f0ccebbf5b9c1f60ef45
Alberto <me@albertorosati.it>
2021-05-21 12:24:26 +0200

    New RicercaUtente Implementation

--- Server/src/ricerca/IRicercaUtente.java
+++ Server/src/ricerca/IRicercaUtente.java
@@ -1,7 +1,10 @@
 package ricerca;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import dominio.Profilo;
 
 public interface IRicercaUtente {
-	public Profilo[] cercaUtente(String name);
+	public List<Profilo> cercaUtente(String name) throws SQLException;
 }
