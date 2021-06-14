55515dc81bd0004d61ef854c56b3022fd1f33db7
LorMolf <lorenzo.molfetta9@gmail.com>
2021-06-14 16:38:30 +0200

    Update Chat.java

--- Server/src/dominio/Chat.java
+++ Server/src/dominio/Chat.java
@@ -13,20 +13,33 @@ import exceptions.EmailNotExistingException;
 public class Chat {
 
 	private int id;
-	private List<Messaggio> messaggi;
+	
+	//getFromDB base on idSegnalazione 
+	private List<Messaggio> messaggiLocal;
+	
+	//messaggi from others
+	private List<Messaggio> messaggiRemote;
 	private int idSegnalazione;
+	
+	//LIST di messaggi aggiornate solo con i nuovi messaggi, non si salvano quelli
+	//passati ma li si passa alla servlet su richiesta in una struttura apposita
+	
+	//Alla fine dell'interazione viene fatta updateChat()
+	//e vengono inseriti nel DB solo i nuovi messaggi
+	
 	
 	//Non serve mantenere due profili. Nel caso di segnalazioni pubbliche qualsiasi gestore può scrivere in chat
 	//obbligatorio salvare sempre Consumatore
 	//Consumatore
 	private Profilo utente1;
-	
+		
 	//Produttore
 	private Profilo utente2;	
 	
 
 	public Chat(int id, Profilo utente1, Profilo utente2, int idSeg) {
-		this.messaggi = new ArrayList<>();
+		this.messaggiLocal = new ArrayList<>();
+		this.messaggiLocal = new ArrayList<>();
 		this.utente1 = utente1;
 		this.utente2 = utente2;
 		this.id=id;
@@ -34,17 +47,69 @@ public class Chat {
 	}
 	
 	public Chat(int id, int consumatore, int idSeg,Connector conn) throws SQLException, EmailNotExistingException {
-		this.messaggi = new ArrayList<>();
+		this.messaggiLocal = new ArrayList<>();
+		this.messaggiLocal = new ArrayList<>();
 		this.utente1 = Profilo.getProfiloById(conn, consumatore);
 		this.utente2 = null;
 		this.id=id;
 		this.idSegnalazione=idSeg;
 	}
 	
-	public static Chat getChat(int idSeg, Connector conn) throws SQLException{
+	
+	public String[][] returnOldMessages(int idProfile, Connector conn) {
+	
+		String [][] res=new String[2][];
+		//LOCAL  Messages...
+		//REMOTE Messages...
 		
-		//return if exists or create new
+		//getFromDB --> messaggi.{Local|Remote}
+		PreparedStatement st;
+		ResultSet rs;
 		
+		List<String> tmp=new ArrayList<>();
+		
+		try {
+			//GET LOCAL MESSAGES
+			st=conn.prepare("SELECT messaggio FROM Messaggi WHERE chat = ? AND mittente = ? ; ");
+			st.setInt(1, this.id);
+			st.setInt(2, idProfile);
+			st.execute();
+			
+			rs = st.executeQuery();
+			
+			while(rs.next()) 
+				tmp.add(rs.getString("messaggio"));
+			
+			res[0]=(String[]) tmp.toArray();
+			
+			tmp.clear();
+			
+			//GET REMOTE MESSAGES
+			st=conn.prepare("SELECT messaggio FROM Messaggi WHERE chat = ? AND mittente != ? ; ");
+			st.setInt(1, this.id);
+			st.setInt(2, idProfile);
+			st.execute();
+			
+			rs = st.executeQuery();
+			
+			while(rs.next()) 
+				tmp.add(rs.getString("messaggio"));
+			
+			res[1]=(String[]) tmp.toArray();
+			
+			tmp.clear();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+				
+		return res;
+	}
+	
+	public static Chat getChat(int idSeg,int idProfile, Connector conn) throws SQLException{
+		
+		//idProfile --> Local
+				
 		int id=-1, idConsumatore=-1;
 		
 		
@@ -56,8 +121,9 @@ public class Chat {
 			st.setInt(1, idSeg);
 			st.execute();
 			
-			rs = st.getGeneratedKeys();
-			
+			rs = st.executeQuery();
+				
+			//return if exists or create new
 			if (!rs.first()) {
 				
 				//GET CONSUMATORE
@@ -74,9 +140,9 @@ public class Chat {
 				st.execute();
 				
 				rs = st.getGeneratedKeys();
-			}else
-				idConsumatore=rs.getInt("consumatore");
-			
+			}else {
+				idConsumatore=rs.getInt("consumatore");				
+			}
 			id = rs.getInt("id");
 			
 		} catch (SQLException e) {
@@ -84,8 +150,8 @@ public class Chat {
 		}
 		
 		//utente1 e 2 da prendere con JOIN Assegnazione
-		
-		Chat res;
+		//utente2 può rimanere null per i motivi di cui sopra
+		Chat res=null;
 		
 		try {
 			res=new Chat(id,idConsumatore,idSeg,conn);
@@ -96,17 +162,59 @@ public class Chat {
 		return res;
 	}
 	
-	public void inviaMessaggio(Messaggio messaggio, Connector conn) {
-		this.messaggi.add(messaggio);
+	public void inviaMessaggio(Messaggio messaggio, int idMittente, Connector conn) {
 		
-		//Update DB:  Messaggi <-- (idChat,mittente,testo)
-		Messaggio.getMessaggio(this.id,messaggio.getTesto(),, conn);
+		/*WEB-SOCKET
+		if(idMittente==this.utente1.getId())
+			this.messaggiLocal.add(messaggio);
+		else
+			this.messaggiRemote.add(messaggio);
 		
+		
+		//Update DB:  Messaggi <-- (idChat,mittente,testo)  al momento della chiusura della pagina
+		//da parte di entrambi gli host
+		*/
+		
+		//POLLING
+		PreparedStatement st;
+		ResultSet rs;
+		
+		try {
+			st=conn.prepare("INSERT INTO Messaggi (chat, mittente, messaggio) VALUES (?,?,?) ; ");
+			st.setInt(1, this.id);
+			st.setInt(2, idMittente);
+			st.setString(3, messaggio.getTesto());
+			st.execute();		
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void updateDB(Messaggio messaggio, Connector conn) {
+		PreparedStatement st;
+		ResultSet rs;
+		
+		
+		List<Messaggio> merge=new ArrayList<>();
+		merge.addAll(this.messaggiLocal);
+		merge.addAll(this.messaggiRemote);
+		
+		//st=conn.prepare("INSERT INTO SET ")
 		
 	}
 
 	public void termina() {
-		this.messaggi.stream().forEach(e -> messaggi.remove(e));
+		this.messaggiLocal.stream().forEach(e -> messaggiLocal.remove(e));
+		this.messaggiRemote.stream().forEach(e -> messaggiRemote.remove(e));
+	}
+	
+	public String[] getMessaggi(int idProfilo) {
+		
+		if(idProfilo==this.getUtente1().getId())
+			return (String[]) this.messaggiLocal.toArray();
+		else
+			return (String[]) this.messaggiRemote.toArray();
+					
 	}
 
 	//-------------------GETTERS & SETTERS---------------------------------	
@@ -119,8 +227,12 @@ public class Chat {
 		return idSegnalazione;
 	}
 
-	public List<Messaggio> getMessaggi() {
-		return messaggi;
+	public List<Messaggio> getAllMessaggi() {
+		List<Messaggio> res=new ArrayList<>();
+		res.addAll(this.messaggiLocal);
+		res.addAll(this.messaggiRemote);
+		
+		return res;
 	}
 
 	public Profilo getUtente1() {
